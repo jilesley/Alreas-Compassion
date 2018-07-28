46f00868c9dcac059a634aee6c0d81302f3678f7
James Ilesley <jilesley@gmail.com>
2018-07-28 12:57:47 +0100

    improved look of menu and added character content

--- resources/content/characters.js
+++ resources/content/characters.js
@@ -1,51 +1,65 @@
-const characters = [
-  {
-    name: "Artiti Baumk",
-    content: "",
-    player: "Goss",
-    race: "Human",
-    class: "Ranger",
-    level: 2
-  },
-  {
-    name: "Belrim Bennar",
-    content: "",
-    player: "Luke",
-    race: "Dwarf",
-    class: "Bard",
-    level: 2
-  },
-  {
-    name: "Hieronymus Botch",
-    content: "",
-    player: "Alex",
-    race: "Human",
-    class: "Monk",
-    level: 2
-  },
-  {
-    name: "Orthon Anatole",
-    content: "",
-    player: "James",
-    race: "Dragonborn",
-    class: "Paldin",
-    level: 2
-  },
-  {
-    name: "Tieron Malathrar",
-    content: "",
-    player: "Nick",
-    race: "Half-Elf",
-    class: "Sorcerer",
-    level: 2
-  },
-  {
-    name: "Varic",
-    content: "",
-    player: "Zac",
-    race: "Goliath",
-    class: "Blood Hunter",
-    level: 2
-  }
-
-];
+const characters = {
+  player: [
+    {
+      name: "Artiti Baumk",
+      player: "Goss",
+      race: "Human",
+      class: "Ranger",
+      level: 2,
+      img: "https://media-waterdeep.cursecdn.com/avatars/thumbnails/17/415/60/60/636377885173419481.jpeg",
+      content: "<h2>Content to be added</h2>"
+    },
+    {
+      name: "Belrim Bennar",
+      player: "Luke",
+      race: "Dwarf",
+      class: "Bard",
+      level: 2,
+      img: "https://media-waterdeep.cursecdn.com/avatars/thumbnails/10/71/60/60/636339380148524382.png",
+      content: "<h2>Content to be added</h2>"
+    },
+    {
+      name: "Hieronymus Botch",
+      player: "Alex",
+      race: "Human",
+      class: "Monk",
+      level: 2,
+      img: "https://media-waterdeep.cursecdn.com/avatars/thumbnails/17/424/60/60/636377886749285983.jpeg",
+      content: "<h2>Content to be added</h2>"
+    },
+    {
+      name: "Orthon Anatole",
+      player: "James",
+      race: "Dragonborn",
+      class: "Paldin",
+      level: 2,
+      img: "https://media-waterdeep.cursecdn.com/avatars/thumbnails/17/407/60/60/636377883694580108.jpeg",
+      content: "<h2>Content to be added</h2>"
+    },
+    {
+      name: "Tieron Malathrar",
+      player: "Nick",
+      race: "Half-Elf",
+      class: "Sorcerer",
+      level: 2,
+      img: "https://media-waterdeep.cursecdn.com/avatars/thumbnails/17/932/60/60/636378855806248172.png",
+      content: "<h2>Content to be added</h2>"
+    },
+    {
+      name: "Varic",
+      player: "Zac",
+      race: "Goliath",
+      class: "Blood Hunter",
+      level: 2,
+      img: "https://media-waterdeep.cursecdn.com/avatars/thumbnails/17/452/60/60/636377890039744108.jpeg",
+      content: "<h2>Content to be added</h2>"
+    }
+  ],
+  npc: [
+    {
+      name: "",
+      race: "",
+      content: ""
+    }
+  ]
+};
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
   let $resources = $("#resources .list");
   let $content = $("#content");
 
+  // Create Menu lists with data attached
+
+  //Quest logs
   for (var i = 0; i < quest.length; i++) {
     let $log = $("<h3>" + quest[i].name + "</h3>");
     $log.data("content", quest[i].content);
@@ -21,20 +24,51 @@ $(document).ready(function() {
     $quest.append($log);
   }
 
-  for (var i = 0; i < characters.length; i++) {
-    let $character = $("<h3>" + characters[i].name + "</h3>");
-    $characters.append($character);
+  //Characters
+  let $charPlayer = $("#charPlayer");
+  let $charNPC = $("#charNPC");
+  for (var i = 0; i < characters.player.length; i++) {
+    let $character = $("<h3>" + characters.player[i].name + "</h3>");
+    $character.data("summary",
+                    "<div class='summary'>" +
+                      "<h2 class='title'>" + characters.player[i].name + "</h2>" +
+                        "<div class='details'>" +
+                        "<img src='" + characters.player[i].img + "' />" +
+                        "<div>" +
+                          "<h4><strong>Player:</strong> " + characters.player[i].player + "</h4>" +
+                          "<h4>Level " +
+                            characters.player[i].level + " | " +
+                            characters.player[i].race + " | " +
+                            characters.player[i].class +
+                          "</h4>" +
+                        "</div>" +
+                      "</div>" +
+                    "</div>"
+    );
+    $character.data("content", characters.player[i].content);
+    $character.click(function() {
+      $content.html($(this).data("summary") + $(this).data("content"));
+    })
+    $charPlayer.append($character);
+  }
+  for (var i = 0; i < characters.npc.length; i++) {
+    let $character = $("<h3>" + characters.npc[i].name + "</h3>");
+    $charNPC.append($character);
   }
 
+  //Locations
   for (var i = 0; i < locations.length; i++) {
     let $location = $("<h3>" + locations[i].name + "</h3>");
     $locations.append($location);
   }
 
+  //Resources
   for (var i = 0; i < resources.length; i++) {
     let $resource = $("<h3><a href='" + resources[i].link + "' target='_blank'>" + resources[i].name + "</a></h3>");
     $resources.append($resource);
   }
+
+  // Give Menu catagories dropdown functionality
 
   $("#menu h2").click(function() {
     $list = $(this).parent().children(".list");
